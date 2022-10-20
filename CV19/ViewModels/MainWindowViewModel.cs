28160855280f5b2a91b1368b77933701ae3e4bd4
Igor <igor.freelance@mail.ru>
2022-10-20 20:34:04 +0600

    FirstStep on learn

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string pTitle;
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title { get => pTitle; set => Set(ref pTitle,value); }
+
+    }
+}
