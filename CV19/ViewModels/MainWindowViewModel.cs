d7dd29edd9d165e79f02527246aa9c813a2b709a
Igor <igor.freelance@mail.ru>
2022-10-22 23:46:47 +0600

    Создал абстрактный класс комнды

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -10,11 +10,21 @@ namespace CV19.ViewModels
     class MainWindowViewModel : ViewModel
     {
         private string pTitle;
-
+         
         /// <summary>
         /// Заголовок окна
         /// </summary>
-        public string Title { get => pTitle; set => Set(ref pTitle,value); }
+        public string Title { get => pTitle; set => Set(ref pTitle, value); }
 
+
+        /// <summary>
+        /// Описание статуса
+        /// </summary>
+        private string pStatus="Мой статус";
+
+        /// <summary>
+        /// Статус программы
+        /// </summary>
+        public string Status { get => pStatus; set => Set(ref pStatus, value); }
     }
 }
